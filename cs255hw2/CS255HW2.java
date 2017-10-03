74b070e65ff01ed5e2fe029790915f1b3aa03190
nicholas.bohm <nicholas.bohm@HU130H004.Blackburn.College>
2017-10-02 21:01:47 -0500

    finished packet payload no idea if it works

--- cs255hw2/CS255HW2.java
+++ cs255hw2/CS255HW2.java
@@ -1,5 +1,5 @@
 package cs255hw2;
-
+import java.io.*;
 /**
  *
  * @author admin.dakota
@@ -10,7 +10,11 @@ public class CS255HW2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new testchecksum();
+        FileHandler pish = new FileHandler();
+        byte[] test = pish.fileTobyte(new File("C:\\Users\\TEMP\\Desktop\\HW2RDT3.0.zip"));
+        System.out.println(test.length);
+        byte[][] test2 = pish.packetPayloadAssembler(test);
+        System.out.println(test2.length);
     }
     
 }
