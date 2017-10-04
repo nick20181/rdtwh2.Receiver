eee56a09196a2a2dfb0632f44665b4e05b8d17df
nicholas.bohm <nicholas.bohm@HU130H004.Blackburn.College>
2017-10-03 20:00:50 -0500

    inlab

--- cs255hw2/Sender/SenderClient.java
+++ cs255hw2/Sender/SenderClient.java
@@ -22,20 +22,23 @@ public class SenderClient {
 
     public SenderClient() {
         try {
-            this.currentSocket = new Socket("10.100.1.239", 1000);
+            this.currentSocket = new Socket("10.15.1.21", 4466);
             this.incoming = new DataInputStream(this.currentSocket.getInputStream());
             this.outGoing = new PrintStream(this.currentSocket.getOutputStream());
             InputStreamReader ir = new InputStreamReader(this.currentSocket.getInputStream());
             this.bufferInput = new BufferedReader(ir);
         } catch (UnknownHostException e) {
-            // System.out.println("SH UHE");
+            System.out.println("SH UHE");
         } catch (IOException ex) {
-            // System.out.println("SH IOE");
+            System.out.println("SH IOE");
         }
-        this.outGoing.println("Test");
     }
 
-    public static void main(String[] args) throws IOException {
-        new SenderClient();
+    public void sendCommand(byte[] pckt) {
+        try {
+            this.currentSocket.getOutputStream().write(pckt);
+        } catch (IOException ex) {
+            System.out.println("SH IOE");
+        }
     }
 }
