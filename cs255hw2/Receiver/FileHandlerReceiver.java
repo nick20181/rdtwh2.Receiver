3552fd7aee16a326fcc8c6e9aeacc72a711b1223
admin.dakota <admin.dakota@HU130H005.Blackburn.College>
2017-10-04 18:36:07 -0500

    Added FileHandlerReceiver

---
+++ cs255hw2/Receiver/FileHandlerReceiver.java
@@ -0,0 +1,43 @@
+package cs255hw2.Receiver;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Nicholas Bohm <your.name at your.org>
+ */
+public class FileHandlerReceiver {
+
+    public FileHandlerReceiver() {
+
+    }
+
+    public void construct(byte[][] pckt) {
+        int counter = 0;
+        File downloadFolder = new File("..");
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(downloadFolder);
+            for (int i = 0; i != pckt.length; i++) {
+                counter = pckt[i].length + counter;
+            }
+            byte[] target = new byte[counter];
+            ByteBuffer buffer = ByteBuffer.allocate(counter);
+            for (int i = 0; i != pckt.length; i++) {
+                buffer.put(pckt[i]);
+            }
+            buffer.flip();
+            buffer.get(target);
+            fos.write(target);
+        } catch (FileNotFoundException ex) {
+            System.out.println("error");
+        } catch (IOException io) {
+            System.out.println("error2");
+        }
+
+    }
+
+}
